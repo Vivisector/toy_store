2017a5ead445e602dad4732f6f311743e8f384f1
Dmitry <hulk295@yandex.ru>
2024-12-14 19:03:54 +0300

    Добавлена админка + поле size к играм. Добавлен список игр с пагинацией. Добавлены представления и шаблоны от ДЗ с регистрацией нового пользователя. Добавлено ограничение по возрасту для поля "Возраст". задание по теме Формы отправки данных. HTML и Django формы

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Game, Buyer  # Импортируем модели
 
 # Register your models here.
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    # Отображаемые поля в списке
+    list_display = ('title', 'price', 'size')
+    # Поля для фильтрации
+    list_filter = ('size', 'price')
+    # Поле для поиска
+    search_fields = ('title',)
+    # Ограничение на количество записей на странице
+    list_per_page = 10
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    # Отображаемые поля в списке
+    list_display = ('username', 'balance', 'age')
+    # Поля для фильтрации
+    list_filter = ('balance', 'age')
+    # Поле для поиска
+    search_fields = ('username',)
+    # Ограничение на количество записей на странице
+    list_per_page = 10
+    # Поле доступно только для чтения
+    readonly_fields = ('balance',)
--- main/models.py
+++ main/models.py
@@ -5,6 +5,7 @@ class Buyer(models.Model):
     username = models.CharField(max_length=30, unique=True)
     password = models.CharField(max_length=20)
     age = models.PositiveIntegerField()
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
     def __str__(self):
         return self.username
@@ -13,6 +14,7 @@ class Game(models.Model):
     title = models.CharField(max_length=100)  # Название игры
     description = models.TextField()  # Описание игры
     price = models.DecimalField(max_digits=10, decimal_places=2)  # Стоимость игры
+    size = models.IntegerField(default=0)
     # release_date = models.DateField()  # Дата выпуска игры (по желанию)
 
     def __str__(self):
